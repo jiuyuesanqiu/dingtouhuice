88fd29d4468729acde888ce7839d35c316c860fc
刘良 <liuliang@tuzhanai.com>
2020-10-16 11:17:48 +0800

    修改配置

--- dingTouHuiCheDjango/settings.py
+++ dingTouHuiCheDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0k!cs+ekp*q@lby8e72nk)l(#=x(ct7dzy93ccxmcswknmla6m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
